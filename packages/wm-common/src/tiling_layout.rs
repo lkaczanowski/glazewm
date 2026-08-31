07257391de1c2a7ab8df22239b7448f27f6efa84
Łukasz Kaczanowski <1639637+lkaczanowski@users.noreply.github.com>
2026-08-31 23:24:52 +0200

    feat(wm): add tiling layout type

---
+++ packages/wm-common/src/tiling_layout.rs
@@ -0,0 +1,14 @@
+use clap::ValueEnum;
+use serde::{Deserialize, Serialize};
+
+/// Automated layout policy for a workspace.
+#[derive(
+  Clone, Copy, Debug, Default, Deserialize, Eq, PartialEq, Serialize, ValueEnum,
+)]
+#[clap(rename_all = "snake_case")]
+#[serde(rename_all = "snake_case")]
+pub enum TilingLayout {
+  #[default]
+  None,
+  Bsp,
+}
