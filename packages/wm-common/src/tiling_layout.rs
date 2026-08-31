7e784d3bb7425fd405dc0bdffb311a21cf4ee11a
Łukasz Kaczanowski <1639637+lkaczanowski@users.noreply.github.com>
2026-08-31 23:35:08 +0200

    style: format BSP layout implementation

--- packages/wm-common/src/tiling_layout.rs
+++ packages/wm-common/src/tiling_layout.rs
@@ -3,7 +3,15 @@ use serde::{Deserialize, Serialize};
 
 /// Automated layout policy for a workspace.
 #[derive(
-  Clone, Copy, Debug, Default, Deserialize, Eq, PartialEq, Serialize, ValueEnum,
+  Clone,
+  Copy,
+  Debug,
+  Default,
+  Deserialize,
+  Eq,
+  PartialEq,
+  Serialize,
+  ValueEnum,
 )]
 #[clap(rename_all = "snake_case")]
 #[serde(rename_all = "snake_case")]
