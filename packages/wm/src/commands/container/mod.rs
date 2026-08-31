210484c1893868494e102ad276186e52c827e4ac
Łukasz Kaczanowski <1639637+lkaczanowski@users.noreply.github.com>
2026-08-31 23:27:16 +0200

    feat(wm): add BSP workspace layout state

--- packages/wm/src/commands/container/mod.rs
+++ packages/wm/src/commands/container/mod.rs
@@ -1,4 +1,5 @@
 mod attach_container;
+mod bsp;
 mod detach_container;
 mod flatten_child_split_containers;
 mod flatten_split_container;
@@ -8,10 +9,12 @@ mod move_container_within_tree;
 mod replace_container;
 mod resize_tiling_container;
 mod set_focused_descendant;
+mod set_tiling_layout;
 mod toggle_tiling_direction;
 mod wrap_in_split_container;
 
 pub use attach_container::*;
+pub use bsp::*;
 pub use detach_container::*;
 pub use flatten_child_split_containers::*;
 pub use flatten_split_container::*;
@@ -21,5 +24,6 @@ pub use move_container_within_tree::*;
 pub use replace_container::*;
 pub use resize_tiling_container::*;
 pub use set_focused_descendant::*;
+pub use set_tiling_layout::*;
 pub use toggle_tiling_direction::*;
 pub use wrap_in_split_container::*;
